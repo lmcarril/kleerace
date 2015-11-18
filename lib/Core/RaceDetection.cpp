a78d127a7425d39d75cf5f6b582028561921c269
Luis M. Carril Rodriguez <luis.rodriguez@kit.edu>
2015-11-18 16:14:26 +0100

    Disable race detection by default to make it consistent with minimal enabled flags

--- lib/Core/RaceDetection.cpp
+++ lib/Core/RaceDetection.cpp
@@ -5,7 +5,7 @@ using namespace llvm;
 
 cl::opt<RaceAlg>
 klee::RaceDetectionAlgorithm("race-detection",
-                       cl::desc("Race detection algorithm (default=hb)"),
+                       cl::desc("Race detection algorithm (default=off)"),
                        cl::values(
                          clEnumValN(None, "off", "Disabled race detection"),
                          clEnumValN(HappensBeforeAlg, "hb", "Happens before"),
@@ -13,4 +13,4 @@ klee::RaceDetectionAlgorithm("race-detection",
                          clEnumValN(LocksetAlg, "ls", "Lockset"),
                          clEnumValN(HybridAlg, "hyb", "Weak happens before with lockset"),
                          clEnumValEnd),
-                       cl::init(HappensBeforeAlg));
+                       cl::init(None));
